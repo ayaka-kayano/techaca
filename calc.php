fdc1b17ffe25cc79a31e63bfa5f6a90a36933597
ayaka <sayonara_setsuna@yahoo.co.jp>
2016-05-16 18:32:14 +0900

    calc追加

---
+++ calc.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>PHPの基本</title>
+</head>
+<body>
+<?php
+//print 命令は指定された文字列を表示するための命令です
+print 5 + 4;
+print 6 - 3;
+print 3 * 4;
+print 15 / 3;
+
+
+?>
+</body>
+</html>
+
