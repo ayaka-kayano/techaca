b6bb241e28f16b7dce58a9a06194625d6629a94b
ayaka <sayonara_setsuna@yahoo.co.jp>
2016-05-25 18:27:24 +0900

    20160525

--- calc3.php
+++ calc3.php
@@ -2,20 +2,20 @@
 <head>
     <title>電卓</title>
 </head>
-<?php
-print '数値を小文字で入力してください'
-?>
+
+<p>'数値を小文字で入力してください'</p>
+
 <body>
 
 <!--フォームを作る-->
-<form action = "calc3.php" method= "POST">
+<form action = 'calc3.php' method= 'POST'>
     <input type = 'text' name ='num1'>  <!--１つ目の項-->
 
     <select name='select'>  <!--演算子の選択-->
-        <option value="+">+</option>
-        <option value="-">-</option>
-        <option value="*">×</option>
-        <option value="/">÷</option>
+        <option value='+'>+</option>
+        <option value='-'>-</option>
+        <option value='*'>×</option>
+        <option value='/'>÷</option>
     </select>
 
     <input type = 'text' name ='num2'><br/>  <!--２つ目の項-->
@@ -32,21 +32,22 @@ print '数値を小文字で入力してください'
         
         //数字かどうかの判定
         if (is_numeric($_POST['num1']) && is_numeric($_POST['num2'])) {
+
             //どの演算記号を選んだかによって条件わけ
             switch ($_POST['select']) {
-                case'+';
+                case'+':
                     print($_POST['num1'] + $_POST['num2']);
                     break;
-                case'-';
+                case'-':
                     print($_POST['num1']- $_POST['num2']);
                     break;
-                case'*';
+                case'*':
                     print($_POST['num1'] * $_POST['num2']);
                     break;
-                case'/';
+                case'/':
                     //２つ目の項が０の時を除外
-                    if ($_POST['num2'] != 0)
-                        print($_POST['num1'] / $_POST['num2']);
+                    if ($_POST['num2'] != 0){
+                        print($_POST['num1'] / $_POST['num2']);}
                     else print '0を分母にすることはできません';
                     break;
             }
